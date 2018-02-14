aebaa3c97651ef68c770266c93821cf49e01d060
Puyan Wei <puyanwei@gmail.com>
2018-02-14 12:24:39 +0000

    function stores the gold, silver and bronze times

--- spec/evil-code-medal-test.js
+++ spec/evil-code-medal-test.js
@@ -9,5 +9,23 @@ describe("#evilCodeMedal", function() {
       );
       expect(evilCodeMedal.gold).toEqual("00:15:00");
     });
+    it("stores the silver medal time", function() {
+      evilCodeMedal = new EvilCodeMedal(
+        "00:30:00",
+        "00:15:00",
+        "00:45:00",
+        "01:15:00"
+      );
+      expect(evilCodeMedal.silver).toEqual("00:45:00");
+    });
+    it("stores the bronze medal time", function() {
+      evilCodeMedal = new EvilCodeMedal(
+        "00:30:00",
+        "00:15:00",
+        "00:45:00",
+        "01:15:00"
+      );
+      expect(evilCodeMedal.bronze).toEqual("01:15:00");
+    });
   });
 });
--- src/evil-code-medal.js
+++ src/evil-code-medal.js
@@ -1,3 +1,5 @@
 function EvilCodeMedal(userTime, gold, silver, bronze) {
   this.gold = gold;
+  this.silver = silver;
+  this.bronze = bronze;
 }
