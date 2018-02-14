555d3f7e30ecac9df1ddc934bab67de41f4c1c5b
Puyan Wei <puyanwei@gmail.com>
2018-02-14 12:25:57 +0000

    refactored tests into one to expect the medal times in

--- spec/evil-code-medal-test.js
+++ spec/evil-code-medal-test.js
@@ -1,6 +1,6 @@
 describe("#evilCodeMedal", function() {
   describe("#initialize", function() {
-    it("stores the gold medal time", function() {
+    it("stores the medal times", function() {
       evilCodeMedal = new EvilCodeMedal(
         "00:30:00",
         "00:15:00",
@@ -8,23 +8,7 @@ describe("#evilCodeMedal", function() {
         "01:15:00"
       );
       expect(evilCodeMedal.gold).toEqual("00:15:00");
-    });
-    it("stores the silver medal time", function() {
-      evilCodeMedal = new EvilCodeMedal(
-        "00:30:00",
-        "00:15:00",
-        "00:45:00",
-        "01:15:00"
-      );
       expect(evilCodeMedal.silver).toEqual("00:45:00");
-    });
-    it("stores the bronze medal time", function() {
-      evilCodeMedal = new EvilCodeMedal(
-        "00:30:00",
-        "00:15:00",
-        "00:45:00",
-        "01:15:00"
-      );
       expect(evilCodeMedal.bronze).toEqual("01:15:00");
     });
   });
